c78b398080bdf2320598fa117ec02009d16c576e
Diane Nguyen <diane.nguyen.124@gmail.com>
2020-10-02 10:36:10 -0700

    Added a details view, tab bar navigation, and grid layout feature

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Diane Nguyen on 10/1/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
